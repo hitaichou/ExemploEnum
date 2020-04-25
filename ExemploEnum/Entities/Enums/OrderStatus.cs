8eb57df57d65d5b4e708d8ce1474abe158c95809
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2020-04-25 09:56:35 -0300

    Criação do tipo enum e exemplo de conversão de string para enum e vice-versa.

---
+++ ExemploEnum/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ExemploEnum.Entities.Enums
+{
+    enum OrderStatus : int //todos os valores serão inteiros
+    {
+        PendingPayment = 0,
+        Processing  = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
--- ExemploEnum/Program.cs
+++ ExemploEnum/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExemploEnum.Entities; 
+using ExemploEnum.Entities.Enums; //chamada do namespace enum
+using System;
 
 namespace ExemploEnum
 {
@@ -6,7 +8,25 @@ namespace ExemploEnum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                //com o enum criado, podemos ver as propridades do OrderStatus já sendo puxadas automaticamente
+                Status = OrderStatus.PendingPayment 
+            };
+            Console.WriteLine(order);
+
+            //conversões
+
+            //conversão do tipo enum (neste caso é int) para texto
+            string txt = OrderStatus.PendingPayment.ToString(); 
+            Console.WriteLine(txt);
+
+            //converter um string para o tipo enumerado(enum)
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); //parse possui parâmetro<tipo> e dentro ("") possui o valor string
+            Console.WriteLine(os);
+        
         }
     }
 }
